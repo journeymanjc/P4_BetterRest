a6c8db4e8de44e77b86633a53c4b74f335f7252a
Jae Cho <jjaecho@gmail.com>
2022-02-05 15:52:24 -0500

    Day 26-2 initial

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -7,15 +7,31 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+	@State private var sleepAmount: Double  = 0
+	@State private var wakeUp = Date.now
+	
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+		 VStack{
+			 Text("Hello, world!")
+				  .padding()
+			 Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+			 DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
+				 .labelsHidden()
+		 }
     }
+	
+	func exampleDates(){
+		let tomorrow = Date.now.addingTimeInterval(86400)
+		let range = Date.now...tomorrow
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+			 .previewDevice("iPhone 13 Mini")
     }
 }
